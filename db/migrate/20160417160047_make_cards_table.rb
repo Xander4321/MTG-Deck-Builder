af7d5bce0015395a5d386576bc9f21d4915b9ec1
Paul <paul@Gregs-MacBook-Pro.local>
2016-04-19 11:30:19 -0400

    add testing and cards table

--- db/migrate/20160417160047_make_cards_table.rb
+++ db/migrate/20160417160047_make_cards_table.rb
@@ -1,4 +1,19 @@
 class MakeCardsTable < ActiveRecord::Migration
   def change
+    create_table :cards do |t|
+      t.string :name, null: false
+      t.string :color_identity, null: false
+      t.string :mana_cost
+      t.integer :cmc, null: false
+      t.string :rarity, null: false
+      t.text :rules_text
+      t.text :flavor_text
+      t.integer :power
+      t.integer :toughness
+      t.integer :loyalty
+      t.belongs_to :artist_id
+      t.integer :number
+      t.text :image
+    end
   end
 end
