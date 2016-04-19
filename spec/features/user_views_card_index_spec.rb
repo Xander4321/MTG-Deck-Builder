e91842fff2cfea07ea3d0c94165b03f557a90aac
Xander4321 <pacornett@gmail.com>
2016-04-19 15:09:09 -0400

    add card index and initial show

---
+++ spec/features/user_views_card_index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'pry'
+
+feature "User visits card index page" do
+  let!(:lotus) {Card.create(
+    name: "Black Lotus",
+    color_identity: "Colorless",
+    mana_cost: "0",
+    cmc: 0,
+    rarity: "Rare"
+    )}
+
+  let!(:timewalk) {Card.create(
+    name: "Time Walk",
+    color_identity: "Blue",
+    mana_cost: "BB",
+    cmc: 2,
+    rarity: "Rare"
+    )}
+
+  scenario "sees list of cards" do
+    visit cards_path
+
+    expect(page).to have_content "Black Lotus"
+    expect(page).to have_content "Time Walk"
+    expect(page).to have_content "0"
+    expect(page).to have_content "BB"
+  end
+end
---
+++ spec/features/user_views_card_show_page_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'pry'
+
+feature "User visits card show page" do
+  let!(:lotus) {Card.create(
+    name: "Black Lotus",
+    color_identity: "Colorless",
+    mana_cost: "0",
+    cmc: 0,
+    rarity: "Rare",
+    rules_text: "{T}, Sacrifice Black Lotus: Add three mana of any one color to your mana pool.",
+    image: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=382866&type=card"
+    )}
+
+  scenario "sees cards details" do
+    visit cards_path
+
+    click_on 'Black Lotus'
+
+    expect(page).to have_content "Black Lotus"
+    expect(page).to have_content "Casting Cost: 0"
+    expect(page).to have_content "Color Identity: Colorless"
+    expect(page).to have_content "Rarity: Rare"
+    expect(page).to have_content "Rules Text: {T}, Sacrifice Black Lotus: Add three mana of any one color to your mana pool."
+    expect(page).to have_selector "img[src$='#{lotus.image}']"
+  end
+end
