31605003849a2044c06fa43d934e34f220f8ea3b
Xander4321 <pacornett@gmail.com>
2016-04-26 13:04:00 -0400

    add card search

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,7 +4,7 @@ class CardsController < ApplicationController
       flash[:error] = "Please enter a search term."
       @cards = Card.order(name: :asc)
     elsif params[:query].present?
-      @cards = Card.search(params[:query])
+      @cards = Card.search("%" + params[:query] + "%")
     else
       @cards = Card.order(name: :asc)
     end
--- app/models/card.rb
+++ app/models/card.rb
@@ -10,9 +10,27 @@ class Card < ActiveRecord::Base
   belongs_to :rarity
 
   include PgSearch
-  pg_search_scope :search,
+  pg_search_scope :search_name,
   against: [:name],
   using: {
     tsearch: { prefix: true, dictionary: "english" }
   }
+
+  pg_search_scope :search_more,
+  associated_against: {
+    supertype: :sptypname,
+    type: :typname,
+    subtype: :sbtypname,
+    expantion: :name,
+    rarity: :value
+  },
+  using: {
+    tsearch: { prefix: true, dictionary: "english" }
+  }
+
+  scope :search, -> (query) do
+    if query.present?
+      search_name(query) + search_more(query)
+    end
+  end
 end
